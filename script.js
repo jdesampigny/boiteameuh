6b55751fa45f2af1bdaa6d057dd5385a6822fc88
jdesampigny <155088808+jdesampigny@users.noreply.github.com>
2024-09-16 00:39:28 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-// Sélectionner l'élément audio
 const audio = document.getElementById('meuhSound');
+const messageElement = document.getElementById('message');
 
-// Détection du mouvement
+// Initialisation du message
+messageElement.textContent = "Secousse non détectée";
+
 window.addEventListener('devicemotion', (event) => {
-    // Ajuster le seuil en fonction de la sensibilité souhaitée
-    const seuil = 10;
+    const seuil = 10; // Ajustez le seuil selon vos besoins
 
-    // Vérifier si l'accélération dépasse le seuil
     if (Math.abs(event.accelerationIncludingGravity.x) > seuil ||
         Math.abs(event.accelerationIncludingGravity.y) > seuil ||
         Math.abs(event.accelerationIncludingGravity.z) > seuil) {
         audio.play();
+        messageElement.textContent = "Secousse détectée !";
     }
 });
